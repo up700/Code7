d3efcc9c99052ebf29229cbb1bfd3085e4ba89ab
up700 <75303154+up700@users.noreply.github.com>
2025-04-01 23:32:35 +0800

    Add files via upload

---
+++ Eval_script/src/__init__.py
@@ -0,0 +1,5 @@
+import os
+os.environ['OPENAI_API_BASE'] = "https://dashscope.aliyuncs.com/compatible-mode/v1/chat/completions"
+os.environ['OPENAI_API_KEY'] = "sk-xxx"
+
+os.environ['DASHSCOPE_API_KEY'] = 'sk-xxx'
